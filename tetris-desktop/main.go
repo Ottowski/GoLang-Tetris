a17b279dc57121f9119fb13296d69d4651438874
Ottowski <115690055+Ottowski@users.noreply.github.com>
2026-01-09 14:18:22 +0100

    changed the starting window size for the programm, so we dont see any scrollbars.

--- tetris-desktop/main.go
+++ tetris-desktop/main.go
@@ -235,8 +235,8 @@ func main() {
 	// Create application with options
 	err = wails.Run(&options.App{
 		Title:  "tetris-desktop",
-		Width:  1024,
-		Height: 768,
+		Width:  1280,
+		Height: 900,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
